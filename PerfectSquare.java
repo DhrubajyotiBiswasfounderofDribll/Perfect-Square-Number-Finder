9c3df5c6fdc8f293a59bbc3e7f016a4a9ce36db5
Dhrubajyoti Biswas <dhrubajyotibiswasDribll@outlook.com>
2025-03-27 21:05:59 +0530

    Create PerfectSquare.java

---
+++ PerfectSquare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PerfectSquare{
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble();
+        double numSqrt = Math.sqrt(num);
+        if(num==numSqrt*numSqrt){
+            System.out.println("It is a perfect square number");
+        }else{
+            System.out.println("It is not a perfect square number");
+        }
+    }
+}
